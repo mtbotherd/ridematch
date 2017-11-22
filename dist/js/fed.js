b777efc6c781bdb19eeda27bc3bbd5461438b2ad
mtbotherd <mtbotherd@users.noreply.github.com>
2017-11-22 14:57:03 -0600

    progress point.

--- dist/js/fed.js
+++ dist/js/fed.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
 
+    // Set class for active link
+    $('.navbar-default a').click(function() {
+        $('.navbar-default a').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    // Toggle clas for days of week
+    $('.days li').click(function() {
+        $(this).toggleClass('active');
+    });
+
 });
--- src/js/fed.js
+++ src/js/fed.js
@@ -3,8 +3,12 @@ $(document).ready(function() {
     // Set class for active link
     $('.navbar-default a').click(function() {
         $('.navbar-default a').removeClass('active');
-        console.log("done!");
         $(this).addClass('active');
+    });
+
+    // Toggle clas for days of week
+    $('.days li').click(function() {
+        $(this).toggleClass('active');
     });
 
 });
