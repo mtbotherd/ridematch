53158970da01b3c7d9dd76e0c1830d72eae65bbf
mtbotherd <mtbotherd@users.noreply.github.com>
2018-01-11 15:56:05 -0600

    Progress point

--- src/js/fed.js
+++ src/js/fed.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
 
     // Set class for active link
-    $('.navbar-default a').click(function() {
-        $('.navbar-default a').removeClass('active');
+    $('.ridematch .navbar-default a').click(function() {
+        $('.ridematch .navbar-default a').removeClass('active');
         $(this).addClass('active');
     });
 
     // Toggle class for days of week
-    $('.days li').click(function() {
+    $('.ridematch .days li').click(function() {
         $(this).toggleClass('active');
     });
 
     // Ridematch message sent confirmation
-    $('#sendMessage').click(function() {
+    $('.ridematch #sendMessage').click(function() {
         $('.modal-body h2, .modal-body .form-group, .modal-footer').hide();
         $('.modal-body p').show();
         return;
